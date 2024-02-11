108c28d7be07a417612990e233d03b280c9d499f
LowSugarCoke <jack20499@gmail.com>
2024-02-11 16:30:45 +0800

    feat: implement LearningTimeLine, AudienceBlock, and TwoColumnLayout components (#11)
    - Implement LearningTimeLine component for the learning map
    - Add AudienceBlock component for displaying learning audience and chips
    - Create TwoColumnLayout for managing left and right component layout
    
    Co-authored-by: Jack Lee <jack.cy.lee@viewsonic.com>

---
+++ src/components/TwoColumnLayout/index.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+export interface TwoColumnLayoutProps {
+  leftColumn: ReactNode;
+  rightColumn: ReactNode;
+}
+
+const TwoColumnLayout = ({ leftColumn, rightColumn }: TwoColumnLayoutProps) => {
+  return (
+    <div className="flex flex-col md:flex-row p-4 md:p-16 w-full md:w-4/5 justify-center items-stretch">
+      <div className="flex-1 md:w-1/2 mb-4 md:mb-0 order-1 md:order-2">
+        {rightColumn}
+      </div>
+      <div className="flex-1 md:w-1/2 order-2 md:order-1">{leftColumn}</div>
+    </div>
+  );
+};
+
+export default TwoColumnLayout;
