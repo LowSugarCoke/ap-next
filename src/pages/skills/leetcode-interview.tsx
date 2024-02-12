bdac37f305493f91da01899366d284bf73ef751f
Jack Lee <jack20499@gmail.com>
2024-02-12 15:52:45 +0800

    feat: update subLink and enhance meta content
    - Replace "競技選手班" with "Android認證競賽班" in the skills page subLink
    - Add detailed description to meta content about the AP course

--- src/pages/skills/leetcode-interview.tsx
+++ src/pages/skills/leetcode-interview.tsx
@@ -10,7 +10,10 @@ export default function LeetcodeInterview() {
     <>
       <Head>
         <title>AP程式 升學專家</title>
-        <meta name="description" content="" />
+        <meta
+          name="description"
+          content="AP課程旨在以最有效率的方式，幫助學生在APCS考試中獲得最高分數。憑藉我們團隊超過10,000小時的APCS家教經驗，我們精心設計了詳細的APCS課程大綱，以實際成績作為我們教學成效的最佳證明。我們提供的是客製化、精緻的教育方案，包括一對一教學、小班課程或自學影片，以滿足不同學生的學習需求。"
+        />
 
         <link rel="icon" href={`${basePath}/Ap_logo_small.ico`} />
       </Head>
